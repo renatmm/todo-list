51efc5f6e618d2474edde51198defb2e2be5b326
Renat Mirzabekov <r.renat0906@mail.ru>
2023-12-14 14:11:15 +0300

    feat: added localStorage usage and updated styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import TodoList from '../TodoList/TodoList';
 import styles from './App.module.css'
@@ -9,31 +9,39 @@ interface IItem {
 }
 
 function App() {
-  const [isDataEmpty, setIsDataEmpty] = useState<boolean>(true)
   const [inputValue, setInputValue] = useState<string>()
   const [todoList, setTodoList] = useState<IItem[]>([]);
 
+  useEffect(() => {
+    const savedTodoList = JSON.parse(localStorage.getItem('todoList') || '[]');
+    setTodoList(savedTodoList);
+  }, []);
+
   const addTodoItem = () => {
-    if (inputValue !== undefined) {
+    if (inputValue) {
       const newItem: IItem = {
         id: uuidv4(),
         name: inputValue,
       };
-      setTodoList([...todoList, newItem]);
+      const newTodoList = [...todoList, newItem];
+      setTodoList(newTodoList);
+      localStorage.setItem('todoList', JSON.stringify(newTodoList)); // Save to localStorage
     }
+    setInputValue("");
   };
 
   const removeTodoItem = (id: string) => {
     const updatedTodoList = todoList.filter(item => item.id !== id);
     setTodoList(updatedTodoList);
+    localStorage.setItem('todoList', JSON.stringify(updatedTodoList)); // Save to localStorage
   };
 
   return (
-      <div className={styles.todo}>
-          <h1 className={styles.todo__title}>Get Things Done!</h1>
-          <input onChange={(e) => setInputValue(e.target.value)} placeholder='What is the task today?' className={styles.todo__input} type="text"/>
-          <button onClick={addTodoItem} className={styles.todo__btn}>Add task</button>
-          {todoList.length > 0 ? <TodoList todoList={todoList} onRemoveItem={removeTodoItem}/> : null}
+      <div className={styles.wrapper}>
+          <h1 className={styles.title}>Get Things Done!</h1>
+          <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder='What is the task today?' className={styles.input} type="text"/>
+          <button onClick={addTodoItem} className={styles.submitBtn}>Add task</button>
+          {todoList.length > 0 && <TodoList todoList={todoList} onRemoveItem={removeTodoItem}/>}
       </div>
   );
 }
--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './TodoItem.module.css';
 
 interface TodoItemProps {
@@ -10,20 +10,37 @@ interface TodoItemProps {
 }
 
 const TodoItem: React.FC<TodoItemProps> = ({ todo, onRemoveItem }) => {
-  const [isTodoItemDone, setIsTodoItemDone] = useState<boolean>(false);
+  const [isTodoItemDone, setIsTodoItemDone] = useState<boolean>(
+    () => {
+      const storedValue = localStorage.getItem(`todo_${todo.id}`);
+      return storedValue ? JSON.parse(storedValue) : false;
+    }
+  );
 
   const handleDelete = (id: string) => {
     onRemoveItem(id); 
   };
 
+  const handleToggleDone = () => {
+    setIsTodoItemDone((prevDone) => {
+      const newDone = !prevDone;
+      localStorage.setItem(`todo_${todo.id}`, JSON.stringify(newDone));
+      return newDone;
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem(`todo_${todo.id}`, JSON.stringify(isTodoItemDone));
+  }, [isTodoItemDone, todo.id]);
+
   return (
-    <li key={todo.id} className={`${styles.todoItem} ${isTodoItemDone ? styles.todoItem_donned : ''}`}>
-      <span className={styles.todoItem__title}>{todo.name}</span>
-      <div className={styles.todoItem__btnWrapper}>
-        <button onClick={() => setIsTodoItemDone(!isTodoItemDone)} className={styles.todoItem__doneBtn}>
-          DONE
+    <li key={todo.id} className={`${styles.todoItem} ${isTodoItemDone ? styles.donned : ''}`}>
+      <span className={styles.title}>{todo.name}</span>
+      <div className={styles.btnsWrapper}>
+        <button onClick={handleToggleDone} className={styles.doneBtn}>
+          {isTodoItemDone ? "UNDONE" : "DONE"}
         </button>
-        <button onClick={() => handleDelete(todo.id)} className={styles.todoItem__delBtn}>
+        <button onClick={() => handleDelete(todo.id)} className={styles.delBtn}>
           DELETE
         </button>
       </div>
